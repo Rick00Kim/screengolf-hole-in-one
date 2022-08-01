91d7f55ed9178426da315306dace7b314525777b
Rick00Kim <dreamx119@gmail.com>
2022-08-01 18:26:01 +0900

    Modify Model index

--- backend/modules/models/index.js
+++ backend/modules/models/index.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.price = require("./prize.data.model.js")(mongoose);
+db.priceData = require("./prize.data.model.js")(mongoose);
 db.changeHistory = require("./change.history.model.js")(mongoose);
 module.exports = db;
