fbc46ab34caff22c86ea2a57e8931f6db934c7ad
Rick00Kim <dreamx119@gmail.com>
2022-08-08 02:47:13 +0900

    Fix wrong word

--- backend/modules/models/index.js
+++ backend/modules/models/index.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.priceData = require("./prize.data.model.js")(mongoose);
+db.prizeData = require("./prize.data.model.js")(mongoose);
 db.changeHistory = require("./change.history.model.js")(mongoose);
 module.exports = db;
