9e5f9c50a26671e0eb0e75c473c5de1f687507b3
Rick00Kim <dreamx119@gmail.com>
2022-08-21 03:39:45 +0900

    Add constants; Backend API result codes

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -2,3 +2,8 @@ export const backendUrl = {
   root: "/api/hole-in-one",
   getLatestHoleInOne: "/api/hole-in-one/latest",
 };
+
+export const backendResultCodes = {
+  SUCCESS: "SUCCESS",
+  FAIL: "FAIL",
+};
